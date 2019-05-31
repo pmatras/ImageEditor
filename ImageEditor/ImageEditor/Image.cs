c56186ec3087a8bf2982ef3f3144a14b6e2bdc9a
birdman98 <piotrmatras21@gmail.com>
2019-05-31 23:34:29 +0200

    added class to handle users image

---
+++ ImageEditor/ImageEditor/Image.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageEditor
+{
+    class UsersImage
+    {
+        // private static string imagePath { public get; public set; }
+        private static string imagePath;
+        public int x { get; private set; }
+
+        public static string getImagePath()
+        {
+            return imagePath;
+        }
+
+        public static void setImagePath(string ImagePath)
+        {
+            imagePath = ImagePath;
+        }
+
+        public void loadImage()
+        {
+           
+        }
+
+        public void makeCopyToEdit()
+        {
+
+        }
+
+        public void saveEditedImage()
+        {
+
+        }
+
+    }
+
+}
+
+
