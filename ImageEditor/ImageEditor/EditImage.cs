b965ed16928b4a9975ade71ac0045b8a3e97c33c
birdman98 <piotrmatras21@gmail.com>
2019-06-01 12:04:17 +0200

    added Interface with editImage() method and classes implementing it

---
+++ ImageEditor/ImageEditor/EditImage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImageEditor
+{
+    interface IEditImage
+    {
+        void editImage(Bitmap imageToEdit);
+    }
+
+    class BlackWhiteEffect : IEditImage
+    {
+        //Dictionary<int, int> ColorToGraynessPixels;
+        public void editImage(Bitmap imageToEdit)
+        {
+            /*ColorToGraynessPixels = new Dictionary<int, int>();
+
+            for(int i = 0; i < 255; ++i)
+            {
+                int newPixelValue = 
+                ColorToGraynessPixels.Add(key: i, value: newPixelValue);
+            }*/
+            Color pixel;
+
+            for(int i = 0; i < imageToEdit.Width; ++i)
+                for(int j = 0; j < imageToEdit.Height; ++j)
+                {
+                    pixel = imageToEdit.GetPixel(i, j);
+
+
+                    var GraynessPixel = 0.299 * pixel.R + 0.587 * pixel.G + 0.114 * pixel.B;
+                    imageToEdit.SetPixel(i, j, Color.FromArgb((int)GraynessPixel, (int)GraynessPixel, (int)GraynessPixel));
+                    
+                }
+
+            UsersImage.saveEditedImage("editedBlackWhite", imageToEdit);
+        }
+    }
+}
